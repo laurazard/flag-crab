dd4f9223276318fa4e95e85a906a823bb659e2cc
Laura Brehm <laurabrehm@hey.com>
2021-01-05 15:14:27 +0000

    :white_check_mark: Add tests for AddFlag web handler

--- src/adapters/web/add_flag_handler.rs
+++ src/adapters/web/add_flag_handler.rs
@@ -35,3 +35,47 @@ pub(crate) fn create_flag(
         .collect();
     Template::render("home", context)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::adapters::persistence::flag_repo::FlagRepo;
+    use crate::adapters::persistence::in_memory_flag_repo::InMemoryFlagRepo;
+    use std::sync::Arc;
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test() {
+        let flag_repo: Box<dyn FlagRepo> = Box::new(InMemoryFlagRepo::new());
+        let flag_repo_mutex = Arc::new(Mutex::new(flag_repo));
+        let get_all_flags_usecase = GetAllFlags::new(Arc::clone(&flag_repo_mutex));
+        let add_flag_usecase = AddFlag::new(Arc::clone(&flag_repo_mutex));
+
+        // FIXME: find a way to do this without waiting a random amount of time for it to be ready
+        thread::spawn(|| {
+            rocket::ignite()
+                .attach(Template::fairing())
+                .manage(get_all_flags_usecase)
+                .manage(Mutex::new(add_flag_usecase))
+                .mount("/", routes![super::create_flag])
+                .launch();
+        });
+        thread::sleep(Duration::from_millis(100));
+
+        let params = [
+            ("name", "a test flag"),
+            ("description", "a test flag for unit tests"),
+            ("enabled", "true"),
+        ];
+        reqwest::blocking::Client::new()
+            .post("http://localhost:8000")
+            .form(&params)
+            .send();
+
+        assert_eq!(
+            flag_repo_mutex.lock().unwrap().get_all_flags()[0].name,
+            String::from("a test flag")
+        );
+    }
+}
