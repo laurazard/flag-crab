ab9b3d05b90fe2da15c41d8fa4152a73a358af6d
Laura Brehm <laurabrehm@hey.com>
2021-01-05 15:23:57 +0000

    :white_check_mark: Add tests for UpdateFlag web handler

--- src/adapters/web/update_flag_handler.rs
+++ src/adapters/web/update_flag_handler.rs
@@ -26,3 +26,42 @@ pub(crate) fn update_flag(
     }
     Redirect::to(format!("/flag/{}", id))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::adapters::persistence::flag_repo::FlagRepo;
+    use crate::adapters::persistence::in_memory_flag_repo::InMemoryFlagRepo;
+    use crate::domain::flag::Flag;
+    use std::sync::Arc;
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test() {
+        let mut flag_repo: Box<dyn FlagRepo> = Box::new(InMemoryFlagRepo::new());
+        flag_repo.add_flag(Flag::new(1, String::from("A test flag name")));
+        let flag_repo_mutex = Arc::new(Mutex::new(flag_repo));
+        let update_flag_usecase = UpdateFlag::new(Arc::clone(&flag_repo_mutex));
+
+        // FIXME: find a way to do this without waiting a random amount of time for it to be ready
+        thread::spawn(|| {
+            rocket::ignite()
+                .manage(Mutex::new(update_flag_usecase))
+                .mount("/", routes![super::update_flag])
+                .launch();
+        });
+        thread::sleep(Duration::from_millis(100));
+
+        let params = [("enabled", "false")];
+        reqwest::blocking::Client::new()
+            .post("http://localhost:8000/flag/1")
+            .form(&params)
+            .send();
+
+        assert_eq!(
+            false,
+            flag_repo_mutex.lock().unwrap().get_all_flags()[0].enabled
+        );
+    }
+}
